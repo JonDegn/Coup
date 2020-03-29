256639f20aeae08ac4fd5b5705c1219c1c0ae50f
Jon Degn <jonathon-degn@pluralsight.com>
2020-03-28 20:36:03 -0600

    Use pixi for graphics

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -1,43 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom'
-
-
-// function Join({ client, setPlayers, setReady }) {
-//     const [name, setName] = useState('')
-//     const [roomName, setRoomName] = useState('')
-
-//     function joinRoom() {
-
-//     }
-
-//     function createRoom() {
-//         client.create('lobby', { name, roomName }).then(r => {
-//             localStorage.setItem("roomId", r.id);
-//             localStorage.setItem("sessionId", r.sessionId);
-
-//             room.onStateChange(s => {
-//                 setPlayers(s.players)
-//                 setReady(s.players[room.sessionId].ready)
-//             })
-//             setSignedIn(true)
-
-//             // props.history.push(`/room/${r.id}`)
-//         })
-//     }
-
-//     return (
-//         <div>
-//             <label>Username
-//             <input type='text' value={name} onChange={e => setName(e.target.value)} />
-//             </label>
-//             <label>Room Name
-//             <input type='text' value={roomName} onChange={e => setRoomName(e.target.value)} />
-//             </label>
-//             <button onClick={createRoom}>Create Room</button>
-//             <button onClick={joinRoom}>Join Room</button>
-//         </div>
-//     )
-// }
+import { Stage, Sprite, Text, Container, Graphics } from '@inlet/react-pixi';
+import * as PIXI from 'pixi.js'
 
 function Main(props) {
     const Colyseus = window.Colyseus
@@ -48,17 +12,26 @@ function Main(props) {
     const [roomName, setRoomName] = useState('')
     const [client, setClient] = useState(null)
     const [room, setRoom] = useState(null)
+    const [currentTurn, setCurrentTurn] = useState(0)
+    const [turnOrder, setTurnOrder] = useState([])
+    const [state, setState] = useState({})
+    // const [pixiApp, setPixiApp] = useState(null)
+    // const [textures, setTextures] = useState({})
+    // const [sprites, setSprites] = useState({})
     // const [loadingRoom, setLoadingRoom] = useState(true)
 
     function connect(r) {
         console.log(r)
         setRoom(r)
         r.onStateChange(s => {
-            console.log("state changed", s.players)
+            console.log("state changed", s)
             setPlayers(s.players)
             setReady(s.players[r.sessionId].ready)
             setInLobby(s.inLobby)
             setRoomName(s.roomName)
+            setCurrentTurn(s.currentTurn)
+            setTurnOrder(s.turnOrder)
+            setState(s)
         })
         localStorage.setItem("roomId", r.id);
         localStorage.setItem("sessionId", r.sessionId);
@@ -75,9 +48,10 @@ function Main(props) {
         var sessionId = localStorage.getItem("sessionId");
 
         client.reconnect(roomId, sessionId).then(r => {
-            console.log("reconnected")
+            console.log("reconnected", r.state)
             connect(r)
         }).catch(() => { })
+
     }, [Colyseus])
 
     function joinRoom() {
@@ -100,6 +74,50 @@ function Main(props) {
         })
     }
 
+    // function setupCanvas() {
+    //     document.body.appendChild(pixiApp.view)
+    //     pixiApp.loader
+    //         .add('ambassadorCard', 'card_ambassador.png')
+    //         .add('assassinCard', 'card_assassin.png')
+    //         .add('captainCard', 'card_captain.png')
+    //         .add('contessaCard', 'card_contessa.png')
+    //         .add('dukeCard', 'card_duke.png')
+    //         .add('cardBack', 'card_back.png')
+    //         .load((loader, resources) => {
+
+    //             setTextures({
+    //                 ambassador: resources.ambassadorCard.texture,
+    //                 assassin: resources.assassinCard.texture,
+    //                 captain: resources.captainCard.texture,
+    //                 contessa: resources.contessaCard.texture,
+    //                 duke: resources.dukeCard.texture,
+    //                 back: resources.cardBack.texture
+    //             })
+    //             let sprites = {}
+    //             Object.keys(players).forEach((pId, i) => {
+    //                 let card1 = new PIXI.Sprite(resources.cardBack.texture)
+    //                 card1.x = i * 2 * resources.cardBack.texture.width
+    //                 let card2 = new PIXI.Sprite(resources.cardBack.texture)
+    //                 card2.x = i * 2 * resources.cardBack.texture.width + resources.cardBack.texture.width
+
+    //                 pixiApp.stage.addChild(card1);
+    //                 pixiApp.stage.addChild(card2);
+
+    //                 sprites[pId] = [card1, card2]
+    //             })
+    //             setSprites(sprites)
+    //             console.log('Sprites', sprites)
+    //         });
+    //         console.log('loaded')
+    // }
+
+    function startGame() {
+        room.send({ command: "startGame" })
+        // setupCanvas()
+    }
+
+    // console.log(state)
+
     return (
         <div>
             {room && players ?
@@ -116,12 +134,26 @@ function Main(props) {
                         )}
                         <button onClick={() => room.send({ command: "toggleReady" })}>{ready ? 'Not Ready' : 'Ready'}</button>
                         {Object.keys(players).filter((pId) => players[pId].ready).length === Object.keys(players).length &&
-                            <button onClick={() => room.send({ command: "startGame" })}>Start Game</button>}
+                            <button onClick={startGame}>Start Game</button>}
                     </>
                     :
+                    //<Game client={client} roomName={roomName} players={players} />
                     <>
-                    <h1>{roomName}</h1>
-                    <div>In Game!</div>
+                        <h1>{roomName}</h1>
+                        <Stage options={{ transparent: true }} width={1080} height={800}>
+                            {turnOrder.map((pId, pNum) => {
+                                const p = players[pId]
+                                return (
+                                    <Container key={pId} x={(256 + 64 )* (pNum % 3) + 32 } y={Math.floor(pNum / 3) * (256)}>
+                                        <Text text={`${pNum === currentTurn? '🟢' : ''}${p.name} ${p.connected? '':' (disconnected)'}`}  y={32} style={new PIXI.TextStyle({fill:`${p.outOfGame? 'gray' : 'black'}`})}/>
+                                        <Text text={`Coins: ${p.coins}`} style={new PIXI.TextStyle({fontSize:16,fill:`${p.outOfGame? 'gray' : 'black'}`})}  y={64}/>
+                                        <Sprite image={`./card_${p.hand[0]}.png`} scale={{ x: 0.5, y: 0.5 }} x={0} y={96} />
+                                        <Sprite image={`./card_${p.hand[1]}.png`} scale={{ x: 0.5, y: 0.5 }} x={128} y={96} />
+                                    </Container>
+                                )
+                            })}
+                        </Stage>
+                        <button onClick={() => room.send({ command: "endTurn" })}>end turn (temp)</button>
                     </>
                 :
                 <>
--- rooms/lobby.ts
+++ rooms/lobby.ts
@@ -1,5 +1,5 @@
 import { Room, Client } from "colyseus";
-import { Schema, MapSchema, type } from "@colyseus/schema";
+import { Schema, MapSchema, ArraySchema, type } from "@colyseus/schema";
 
 
 export class Player extends Schema {
@@ -12,9 +12,21 @@ export class Player extends Schema {
   @type("boolean")
   connected: boolean = true
 
+  @type("number")
+  coins: number = 2
+
+  @type("boolean")
+  outOfGame: boolean = false
+
+  @type(["string"])
+  hand: ArraySchema<string> = new ArraySchema<string>()
+
   constructor(name: string) {
     super()
     this.name = name;
+    const cards = ['card','ambassador','assassin','captain','contessa','duke']
+    this.hand.push(cards[Math.floor(Math.random() * cards.length)])
+    this.hand.push(cards[Math.floor(Math.random() * cards.length)])
   }
 }
 
@@ -27,6 +39,12 @@ export class State extends Schema {
 
   @type("boolean")
   inLobby: boolean = true
+
+  @type(["string"])
+  turnOrder: ArraySchema<string>
+
+  @type("number")
+  currentTurn: number = 0
 
   createPlayer(id: string, name: string) {
     this.players[id] = new Player(name)
@@ -44,7 +62,7 @@ export class State extends Schema {
 export class Lobby extends Room<State> {
   // Colyseus will invoke when creating the room instance
   onCreate(options: any) {
-    this.setMetadata({roomName: options.roomName})
+    this.setMetadata({ roomName: options.roomName })
     console.log("lobby created", options)
     this.setState(new State())
     this.state.roomName = options.roomName
@@ -66,7 +84,9 @@ export class Lobby extends Room<State> {
       this.state.players[client.sessionId].connected = true
     } catch (e) {
       console.log(`${this.state.players[client.sessionId].name} removed from state`)
-      this.state.removePlayer(client.sessionId)
+      // todo: Player forfeits
+      this.state.players[client.sessionId].outOfGame = true
+      // this.state.removePlayer(client.sessionId)
     }
 
     // this.state.removePlayer(client.sessionId)
@@ -79,9 +99,14 @@ export class Lobby extends Room<State> {
     if (message.command === "toggleReady") {
       this.state.toggleReady(client.sessionId)
     } else if (message.command === "startGame") {
+      this.state.turnOrder = new ArraySchema<string>(...Object.keys(this.state.players).sort())
       this.state.inLobby = false
     } else if (message.command === "renamePlayer") {
       this.state.players[client.sessionId].name = message.name
+    } else if (message.command === "endTurn") {
+      do {
+        this.state.currentTurn = (this.state.currentTurn + 1) % this.state.turnOrder.length
+      } while (this.state.players[this.state.turnOrder[this.state.currentTurn]].outOfGame)
     }
   }
 }
