301d6bdfbab8d14d8cc103f0af5f6bc040fc0e4f
Jon Degn <jonathon-degn@pluralsight.com>
2020-03-29 19:51:30 -0600

    add countdown timer to game start

--- client/src/routes/main.js
+++ client/src/routes/main.js
@@ -5,7 +5,6 @@ import * as PIXI from 'pixi.js'
 
 function Main(props) {
     const Colyseus = window.Colyseus
-    const [ready, setReady] = useState(false)
     const [inLobby, setInLobby] = useState(false)
     const [players, setPlayers] = useState({})
     const [name, setName] = useState('')
@@ -14,24 +13,21 @@ function Main(props) {
     const [room, setRoom] = useState(null)
     const [currentTurn, setCurrentTurn] = useState(0)
     const [turnOrder, setTurnOrder] = useState([])
-    const [state, setState] = useState({})
-    // const [pixiApp, setPixiApp] = useState(null)
-    // const [textures, setTextures] = useState({})
-    // const [sprites, setSprites] = useState({})
-    // const [loadingRoom, setLoadingRoom] = useState(true)
+    const [playerId, setPlayerId] = useState('')
+    const [countdownTimer, setCountdownTimer] = useState(0)
 
     function connect(r) {
         console.log(r)
+        setPlayerId(r.sessionId)
         setRoom(r)
         r.onStateChange(s => {
             console.log("state changed", s)
             setPlayers(s.players)
-            setReady(s.players[r.sessionId].ready)
             setInLobby(s.inLobby)
             setRoomName(s.roomName)
             setCurrentTurn(s.currentTurn)
             setTurnOrder(s.turnOrder)
-            setState(s)
+            setCountdownTimer(s.countdownTimer)
         })
         localStorage.setItem("roomId", r.id);
         localStorage.setItem("sessionId", r.sessionId);
@@ -74,49 +70,9 @@ function Main(props) {
         })
     }
 
-    // function setupCanvas() {
-    //     document.body.appendChild(pixiApp.view)
-    //     pixiApp.loader
-    //         .add('ambassadorCard', 'card_ambassador.png')
-    //         .add('assassinCard', 'card_assassin.png')
-    //         .add('captainCard', 'card_captain.png')
-    //         .add('contessaCard', 'card_contessa.png')
-    //         .add('dukeCard', 'card_duke.png')
-    //         .add('cardBack', 'card_back.png')
-    //         .load((loader, resources) => {
-
-    //             setTextures({
-    //                 ambassador: resources.ambassadorCard.texture,
-    //                 assassin: resources.assassinCard.texture,
-    //                 captain: resources.captainCard.texture,
-    //                 contessa: resources.contessaCard.texture,
-    //                 duke: resources.dukeCard.texture,
-    //                 back: resources.cardBack.texture
-    //             })
-    //             let sprites = {}
-    //             Object.keys(players).forEach((pId, i) => {
-    //                 let card1 = new PIXI.Sprite(resources.cardBack.texture)
-    //                 card1.x = i * 2 * resources.cardBack.texture.width
-    //                 let card2 = new PIXI.Sprite(resources.cardBack.texture)
-    //                 card2.x = i * 2 * resources.cardBack.texture.width + resources.cardBack.texture.width
-
-    //                 pixiApp.stage.addChild(card1);
-    //                 pixiApp.stage.addChild(card2);
-
-    //                 sprites[pId] = [card1, card2]
-    //             })
-    //             setSprites(sprites)
-    //             console.log('Sprites', sprites)
-    //         });
-    //         console.log('loaded')
-    // }
-
     function startGame() {
         room.send({ command: "startGame" })
-        // setupCanvas()
     }
-
-    // console.log(state)
 
     return (
         <div>
@@ -132,27 +88,53 @@ function Main(props) {
                             )
                         }
                         )}
-                        <button onClick={() => room.send({ command: "toggleReady" })}>{ready ? 'Not Ready' : 'Ready'}</button>
-                        {Object.keys(players).filter((pId) => players[pId].ready).length === Object.keys(players).length &&
+                        <button onClick={() => room.send({ command: "toggleReady" })}>{players[playerId].ready ? 'Not Ready' : 'Ready'}</button>
+                        {countdownTimer
+                            ? <div>Starting game... {countdownTimer}</div>
+                            : Object.keys(players).filter((pId) => players[pId].ready).length === Object.keys(players).length &&
                             <button onClick={startGame}>Start Game</button>}
                     </>
                     :
-                    //<Game client={client} roomName={roomName} players={players} />
                     <>
                         <h1>{roomName}</h1>
-                        <Stage options={{ transparent: true }} width={1080} height={800}>
+                        {/* <Stage options={{ transparent: true }} width={1080} height={800}>
                             {turnOrder.map((pId, pNum) => {
                                 const p = players[pId]
                                 return (
-                                    <Container key={pId} x={(256 + 64 )* (pNum % 3) + 32 } y={Math.floor(pNum / 3) * (256)}>
-                                        <Text text={`${pNum === currentTurn? '🟢' : ''}${p.name} ${p.connected? '':' (disconnected)'}`}  y={32} style={new PIXI.TextStyle({fill:`${p.outOfGame? 'gray' : 'black'}`})}/>
-                                        <Text text={`Coins: ${p.coins}`} style={new PIXI.TextStyle({fontSize:16,fill:`${p.outOfGame? 'gray' : 'black'}`})}  y={64}/>
+                                    <Container key={pId} x={(256 + 64) * (pNum % 3) + 32} y={Math.floor(pNum / 3) * (256)}>
+                                        <Text text={`${pNum === currentTurn ? '🟢' : ''}${p.name} ${p.connected ? '' : ' (disconnected)'}`} y={32} style={new PIXI.TextStyle({ fill: `${p.outOfGame ? 'gray' : 'black'}` })} />
+                                        <Text text={`Coins: ${p.coins}`} style={new PIXI.TextStyle({ fontSize: 16, fill: `${p.outOfGame ? 'gray' : 'black'}` })} y={64} />
                                         <Sprite image={`./card_${p.hand[0]}.png`} scale={{ x: 0.5, y: 0.5 }} x={0} y={96} />
                                         <Sprite image={`./card_${p.hand[1]}.png`} scale={{ x: 0.5, y: 0.5 }} x={128} y={96} />
                                     </Container>
                                 )
                             })}
-                        </Stage>
+                        </Stage> */}
+                        <table>
+                            <tr><td>Current Turn</td><td>Player</td><td>Coins</td><td>Lost Influence</td></tr>
+                            {turnOrder.map((pId, pNum) => {
+                                const p = players[pId]
+                                return (
+                                    <tr>
+                                        <td>{pNum === currentTurn ? 'X' : ''}</td>
+                                        <td>{`${p.name} ${p.connected ? '' : ' (disconnected)'}`}</td>
+                                        <td>{p.coins}</td>
+                                        <td>cards here</td>
+                                    </tr>
+                                )
+                            })}
+                        </table>
+                        <h2>Actions:</h2>
+                        <button onClick={() => room.send({ command: "action", action: "income" })}>Income +1</button>
+                        <button onClick={() => room.send({ command: "action", action: "foreignAid" })}>Foreign Aid +2</button>
+                        <button onClick={() => room.send({ command: "action", action: "tax" })}>Tax +3</button>
+                        <button onClick={() => room.send({ command: "action", action: "steal", target: "" })}>Steal +2</button>
+                        <button onClick={() => room.send({ command: "action", action: "assassinate", target: "" })}>Assassinate</button>
+                        <button onClick={() => room.send({ command: "action", action: "exchange" })}>Exchange</button>
+                        <h2>Counter-actions:</h2>
+                        <button onClick={() => room.send({ command: "counterAction", action: "steal" })}>Block Stealing</button>
+                        <button onClick={() => room.send({ command: "counterAction", action: "foreignAid" })}>Block Foreign Aid</button>
+                        <button onClick={() => room.send({ command: "counterAction", action: "assassinate" })}>Block Assassination</button>
                         <button onClick={() => room.send({ command: "endTurn" })}>end turn (temp)</button>
                     </>
                 :
--- rooms/lobby.ts
+++ rooms/lobby.ts
@@ -1,4 +1,4 @@
-import { Room, Client } from "colyseus";
+import { Room, Client, Delayed } from "colyseus";
 import { Schema, MapSchema, ArraySchema, type } from "@colyseus/schema";
 
 
@@ -24,7 +24,7 @@ export class Player extends Schema {
   constructor(name: string) {
     super()
     this.name = name;
-    const cards = ['card','ambassador','assassin','captain','contessa','duke']
+    const cards = ['back', 'ambassador', 'assassin', 'captain', 'contessa', 'duke']
     this.hand.push(cards[Math.floor(Math.random() * cards.length)])
     this.hand.push(cards[Math.floor(Math.random() * cards.length)])
   }
@@ -46,6 +46,9 @@ export class State extends Schema {
   @type("number")
   currentTurn: number = 0
 
+  @type("number")
+  countdownTimer: number = 0
+
   createPlayer(id: string, name: string) {
     this.players[id] = new Player(name)
   }
@@ -60,12 +63,16 @@ export class State extends Schema {
 }
 
 export class Lobby extends Room<State> {
+
+  timerInterval: Delayed
+
   // Colyseus will invoke when creating the room instance
   onCreate(options: any) {
     this.setMetadata({ roomName: options.roomName })
     console.log("lobby created", options)
     this.setState(new State())
     this.state.roomName = options.roomName
+    this.clock.start()
   }
 
   // Called every time a client joins
@@ -83,7 +90,7 @@ export class Lobby extends Room<State> {
       console.log(`${this.state.players[client.sessionId].name} reconnected`)
       this.state.players[client.sessionId].connected = true
     } catch (e) {
-      console.log(`${this.state.players[client.sessionId].name} removed from state`)
+      console.log(`${this.state.players[client.sessionId].name} forfeits`)
       // todo: Player forfeits
       this.state.players[client.sessionId].outOfGame = true
       // this.state.removePlayer(client.sessionId)
@@ -98,9 +105,13 @@ export class Lobby extends Room<State> {
 
     if (message.command === "toggleReady") {
       this.state.toggleReady(client.sessionId)
+      if (this.state.players[client.sessionId].ready === false) this.cancelTimer()
     } else if (message.command === "startGame") {
-      this.state.turnOrder = new ArraySchema<string>(...Object.keys(this.state.players).sort())
-      this.state.inLobby = false
+      if (this.timerInterval && this.timerInterval.active) return
+      this.startTimer(5, () => {
+        this.state.turnOrder = new ArraySchema<string>(...Object.keys(this.state.players).sort())
+        this.state.inLobby = false
+      })
     } else if (message.command === "renamePlayer") {
       this.state.players[client.sessionId].name = message.name
     } else if (message.command === "endTurn") {
@@ -108,5 +119,25 @@ export class Lobby extends Room<State> {
         this.state.currentTurn = (this.state.currentTurn + 1) % this.state.turnOrder.length
       } while (this.state.players[this.state.turnOrder[this.state.currentTurn]].outOfGame)
     }
+  }
+
+  startTimer(length: number, callback: Function) {
+    this.state.countdownTimer = length
+    this.timerInterval = this.clock.setInterval(() => {
+      console.log("Timer: ", this.state.countdownTimer)
+      this.state.countdownTimer--
+      if (this.state.countdownTimer == 0) {
+        callback()
+        this.cancelTimer()
+      }
+    }, 1000)
+  }
+
+  cancelTimer() {
+    if (this.timerInterval) {
+      this.timerInterval.clear();
+      console.log("timer cancelled")
+    }
+    this.state.countdownTimer = 0
   }
 }
