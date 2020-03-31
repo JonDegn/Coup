d243a298233e10e01343084f688f4ef208b82db2
Jon Degn <jonathon-degn@pluralsight.com>
2020-03-31 00:09:01 -0600

    More work on rules

--- rules/coup.test.ts
+++ rules/coup.test.ts
@@ -1,26 +1,224 @@
-import { Coup, Deck, Character, PlayerData, Hand } from "./coup"
+import { Coup, Deck, Character, PlayerData, Hand, ActionType, CurrentAction } from "./coup"
 
 
 
-// describe('Coup', () => {
+describe('Coup', () => {
 
-//     it('adds players on creation', () => {
-//         const coup = new Coup(['bob'])
-//         expect(coup.players.length).toEqual(1)
-//     })
 
-//     it('has correct player names', () => {
-//         const coup = new Coup(['bob', 'joe'])
-//         expect(coup.players[0].name).toEqual('bob')
-//         expect(coup.players[1].name).toEqual('joe')
-//     })
+    it('adds players on creation', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(Object.keys(coup.players).length).toEqual(Object.keys(expectedPlayers).length)
+        expect(coup.players.id1.name).toEqual(expectedPlayers.id1.name)
+        expect(coup.players.id2.name).toEqual(expectedPlayers.id2.name)
+    })
 
-//     it('adds players with no hand', () => {
-//         const coup = new Coup(['bob'])
-//         expect(coup.players[0].hand.cards.length).toEqual(0)
-//     })
+    it('deals 2 cards to each player', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.players.id1.hand.cards).toHaveLength(2)
+        expect(coup.players.id1.hand.lostInfluence[0]).toEqual(false)
+        expect(coup.players.id1.hand.lostInfluence[1]).toEqual(false)
+        expect(coup.players.id2.hand.cards).toHaveLength(2)
+        expect(coup.players.id2.hand.lostInfluence[0]).toEqual(false)
+        expect(coup.players.id2.hand.lostInfluence[1]).toEqual(false)
+        expect(coup.deck.cards).toHaveLength(15 - Object.keys(coup.players).length * 2) // Deck should have 15 - 2 players * 2 cards
+    })
 
-// })
+    it('has 15 cards in the deck with 3 players', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            },
+            id3: {
+                name: "Billy"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.deck.cards).toHaveLength(15 - Object.keys(coup.players).length * 2) // 15 - 3 players * 2 cards
+    })
+
+    it('has 15 cards in the deck with 6 players', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            },
+            id3: {
+                name: "Billy"
+            },
+            id4: {
+                name: "Jane"
+            },
+            id5: {
+                name: "Beth"
+            },
+            id6: {
+                name: "Kyle"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.deck.cards).toHaveLength(15 - Object.keys(coup.players).length * 2) // 15 - 6 players * 2 cards
+    })
+
+    it('has 20 cards in the deck with 7 players', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            },
+            id3: {
+                name: "Billy"
+            },
+            id4: {
+                name: "Jane"
+            },
+            id5: {
+                name: "Beth"
+            },
+            id6: {
+                name: "Kyle"
+            },
+            id7: {
+                name: "Josh"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.deck.cards).toHaveLength(20 - Object.keys(coup.players).length * 2) // 20 - 7 players * 2 cards
+    })
+
+    it('has 25 cards in the deck with 9 players', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            },
+            id3: {
+                name: "Billy"
+            },
+            id4: {
+                name: "Jane"
+            },
+            id5: {
+                name: "Beth"
+            },
+            id6: {
+                name: "Kyle"
+            },
+            id7: {
+                name: "Josh"
+            },
+            id8: {
+                name: "Andy"
+            },
+            id9: {
+                name: "Jon"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.deck.cards).toHaveLength(25 - Object.keys(coup.players).length * 2) // 25 - 9 players * 2 cards
+    })
+
+    it('creates turn order', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        expect(coup.turnOrder).toEqual(["id1", "id2"])
+    })
+
+    it('performs action: income', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        const response = coup.addAction(ActionType.Income, 'id1');
+        expect(response.canChallenge).toEqual(false)
+        expect(response.charactersThatCanBlock).toHaveLength(0)
+        expect(response.playersThatCanBlock).toHaveLength(0)
+        expect(coup.actionStack[0].action).toEqual(ActionType.Income)
+        coup.resolveAction()
+        expect(coup.players.id1.coins).toEqual(3)
+        expect(coup.actionStack).toHaveLength(0)
+    }) 
+
+    it('performs action: foreign aid', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        const response = coup.addAction(ActionType.ForeignAid, 'id1');
+        expect(response.canChallenge).toEqual(false)
+        expect(response.charactersThatCanBlock).toEqual([Character.Duke])
+        expect(response.playersThatCanBlock).toEqual(['id2'])
+        expect(coup.actionStack[coup.actionStack.length-1].action).toEqual(ActionType.ForeignAid)
+        coup.resolveAction()
+        expect(coup.players.id1.coins).toEqual(4)
+        expect(coup.actionStack).toHaveLength(0)
+    })
+
+    it('performs counter-action: block foreign aid', () => {
+        const expectedPlayers = {
+            id1: {
+                name: "Bob"
+            },
+            id2: {
+                name: "Joe"
+            }
+        }
+        const coup = new Coup(expectedPlayers)
+        coup.addAction(ActionType.ForeignAid, 'id1');
+        const response = coup.addAction(ActionType.Block, 'id2', 'id1', Character.Duke);
+
+        expect(response.canChallenge).toEqual(true)
+        expect(response.charactersThatCanBlock).toEqual([])
+        expect(response.playersThatCanBlock).toHaveLength(0)
+        expect(coup.actionStack).toHaveLength(2)
+        expect(coup.actionStack[coup.actionStack.length-1].action).toEqual(ActionType.Block)
+        coup.resolveAction()
+        expect(coup.players.id1.coins).toEqual(2)
+        expect(coup.actionStack).toHaveLength(0)
+    }) 
+
+})
 
 describe('Player', () => {
 
@@ -35,7 +233,7 @@ describe('Player', () => {
         const card2 = Character.Contessa
 
         const player = new PlayerData('id', 'Bob', new Hand(card1, card2))
-        
+
         expect(player.hand).toBeDefined()
         expect(player.hand.cards).toHaveLength(2)
         expect(player.hand.cards[0]).toEqual(card1)
--- rules/coup.ts
+++ rules/coup.ts
@@ -1,4 +1,4 @@
-import {Player} from '../rooms/lobby'
+import { Player } from '../rooms/lobby'
 import { Schema, MapSchema, type } from "@colyseus/schema";
 
 
@@ -10,15 +10,81 @@ export enum Character {
     Ambassador
 }
 
-export class Hand {
-    cards:Character[] = []
-    lostInfluence:boolean[] = []
+export enum ActionType {
+    Income,
+    ForeignAid,
+    Tax,
+    Steal,
+    Exchange,
+    Assassinate,
+    Block,
+    Challenge
+}
 
-    constructor(card1:Character, card2:Character) {
+// export enum CounterAction {
+//     BlockForeignAid,
+//     BlockSteal,
+//     BlockAssassination
+// }
+
+export class CounterAction {
+    blockingPlayerId: string
+    blockingCharacter: Character
+    bluff: boolean = false
+    challengingPlayerId: string
+
+    constructor(blockingPlayerId: string, blockingCharacter: Character, bluff: boolean) {
+        this.blockingPlayerId = blockingPlayerId
+        this.blockingCharacter = blockingCharacter
+        this.bluff = bluff
+    }
+}
+
+export class CurrentAction {
+    sourcePlayerId: string
+    targetPlayerId: string
+    action: ActionType
+    execute: () => void
+    asCharacter: Character
+    blocked: boolean = false
+    bluff: boolean = false
+    challengingPlayerId: string
+
+    constructor(action: ActionType, sourcePlayerId: string, targetPlayerId: string, callback: () => void, asCharacter?: Character, bluff?: boolean) {
+        this.action = action
+        this.sourcePlayerId = sourcePlayerId
+        this.targetPlayerId = targetPlayerId
+        this.execute = callback
+        this.asCharacter = asCharacter
+        this.bluff = bluff
+    }
+}
+
+export class ActionResponse {
+    playersThatCanBlock: string[] = []
+    charactersThatCanBlock: Character[] = []
+    canChallenge: boolean
+    constructor(playersThatCanAct: string[], charactersThatCanBlock: Character[], canChallenge: boolean) {
+        this.playersThatCanBlock = playersThatCanAct
+        this.charactersThatCanBlock = charactersThatCanBlock
+        this.canChallenge = canChallenge
+    }
+}
+
+export class Hand {
+    cards: Character[] = []
+    lostInfluence: boolean[] = []
+
+    constructor(card1: Character, card2: Character) {
         this.cards.push(card1)
         this.cards.push(card2)
         this.lostInfluence.push(false)
         this.lostInfluence.push(false)
+    }
+
+    contains(character: Character) {
+        return (!this.lostInfluence[0] && this.cards[0] === character)
+            || (!this.lostInfluence[1] && this.cards[1] === character)
     }
 }
 
@@ -26,9 +92,9 @@ export class PlayerData {
     id: string
     name: string
     hand: Hand
-    lostInfluence: boolean[] = []
+    coins: number = 2
 
-    constructor(id:string, name: string, hand: Hand) {
+    constructor(id: string, name: string, hand: Hand) {
         this.id = id
         this.name = name
         this.hand = hand
@@ -37,7 +103,7 @@ export class PlayerData {
 
 export class Deck {
     cards: Character[] = []
-    constructor(numOfEachSuit:Number) {
+    constructor(numOfEachSuit: Number) {
         for (let i = 0; i < numOfEachSuit; i++) {
             this.cards.push(Character.Ambassador)
             this.cards.push(Character.Assassin)
@@ -57,7 +123,7 @@ export class Deck {
         return this.cards;
     }
 
-    replaceAndShuffle(card:Character) {
+    replaceAndShuffle(card: Character) {
         this.cards.push(card)
         this.shuffle()
     }
@@ -69,11 +135,95 @@ export class Deck {
 }
 
 export class Coup {
-    players: PlayerData[] = []
+    players: any = {}
     deck: Deck
+    currentTurn: number = 0
+    turnOrder: string[] = []
+    // currentAction: CurrentAction = null
+    // counterAction: CounterAction = null
+    actionStack: CurrentAction[] = []
 
     constructor(players) {
-        // this.players = playerNames.map(n => new PlayerData(n))
-        this.deck = new Deck(1)
+        const playerIds = Object.keys(players)
+        this.turnOrder = playerIds.sort()
+        const numOfEachCharacter = playerIds.length < 7
+            ? 3
+            : playerIds.length < 9
+                ? 4
+                : 5;
+        this.deck = new Deck(numOfEachCharacter)
+        playerIds.forEach(pId => {
+            const p = players[pId]
+            const hand = new Hand(this.deck.drawCard(), this.deck.drawCard())
+            this.players[pId] = new PlayerData(pId, p.name, hand)
+        })
     }
+
+    currentPlayer(): PlayerData {
+        return this.players[this.turnOrder[this.currentTurn]]
+    }
+
+    playerHandContains(playerId: string, character: Character) {
+        return this.players[playerId].hand.contains(character)
+    }
+
+    addAction(action: ActionType, sourcePlayerId: string, targetPlayerId?: string, asCharacter?: Character): ActionResponse {
+        if (action === ActionType.Income) {
+            this.actionStack.push(new CurrentAction(action, sourcePlayerId, targetPlayerId, () => this.currentPlayer().coins++))
+            return new ActionResponse([], [], false)
+        } else if (action === ActionType.ForeignAid) {
+            this.actionStack.push(new CurrentAction(action, sourcePlayerId, targetPlayerId, () => this.currentPlayer().coins += 2))
+            return new ActionResponse(Object.keys(this.players).filter(pId => pId != this.currentPlayer().id), [Character.Duke], false) // todo: should only be players still in game
+        } else if (action === ActionType.Block) {
+            this.actionStack.push(new CurrentAction(action, sourcePlayerId, targetPlayerId, () => {
+                console.log(`${this.players[sourcePlayerId].name} blocked ${this.currentPlayer().name}`)
+                this.actionStack.pop()
+            }, asCharacter, !this.playerHandContains(sourcePlayerId, asCharacter)))
+            return new ActionResponse([], [], true)
+        }
+        return new ActionResponse([], [], false)
+    }
+
+    resolveAction() {
+        const action = this.actionStack.pop()
+        action.execute()
+    }
+
+    // startAction(action: ActionType, targetPlayerId?: string) {
+    //     console.log(`${this.currentPlayer().name} is doing action ${action}, targeting ${targetPlayerId ? this.players[targetPlayerId].name : "no one"}`)
+
+    //     if (action === ActionType.Income) {
+    //         this.currentAction = new CurrentAction(action, targetPlayerId, () => this.currentPlayer().coins++, [], false)
+    //     } else if (action === ActionType.ForeignAid) {
+    //         this.currentAction = new CurrentAction(action, targetPlayerId, () => this.currentPlayer().coins += 2, [Character.Duke], false)
+    //     }
+    // }
+
+    // challengeAction(challengingPlayerId: string) {
+    //     console.log(`${this.players[challengingPlayerId].name} challenges ${this.currentPlayer().name}`)
+    //     this.currentAction.challengingPlayerId = true
+    // }
+
+    // challengeCounterAction(challengingPlayerId: string) {
+    //     console.log(`${this.players[challengingPlayerId].name} challenges ${this.currentPlayer().name}`)
+    //     this.counterAction.challengingPlayerId = true
+    // }
+
+    // blockAction(blockingPlayerId: string, character: Character) {
+    //     this.counterAction = new CounterAction(blockingPlayerId, character, false)
+    // }
+
+    // resolveAction() {
+    //     if (this.counterAction) {
+    //         if(this.counterAction.challengingPlayerId) {
+    //             if(this.counterAction.bluff) {
+    //                 // 
+    //                 //this.players[this.counterAction.blockingPlayerId]
+    //             }
+    //         }
+    //     }
+
+    //     this.currentAction.execute()
+    //     this.currentAction = null;
+    // }
 }
